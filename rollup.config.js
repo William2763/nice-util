3b2289f7cca55d74ce3f82cc9ebff7b39abc2a37
will <William2763@proton.me>
2024-05-25 14:48:23 +0800

    feat: optimize buid

--- rollup.config.js
+++ rollup.config.js
@@ -1,41 +1,51 @@
-// rollup.config.js
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import json from '@rollup/plugin-json';
 import { terser } from 'rollup-plugin-terser';
 
-export default {
-  input: {
-    index: 'src/index.ts'
+export default [
+  // ESM and CJS configuration
+  {
+    input: ['src/index.ts'],
+    output: [
+      {
+        dir: 'dist/esm',
+        format: 'esm',
+        entryFileNames: '[name].js'
+      },
+      {
+        dir: 'dist/cjs',
+        format: 'cjs',
+        entryFileNames: '[name].js'
+      }
+    ],
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      typescript(),
+      json()
+    ],
+    preserveModules: true,
   },
-  output: [
-    {
-      dir: 'dist/esm',
-      format: 'esm',
-      entryFileNames: '[name].js'
-    },
-    {
-      dir: 'dist/cjs',
-      format: 'cjs',
-      entryFileNames: '[name].js'
-    },
-    {
-      dir: 'dist/umd',
+  // UMD configuration
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/umd/index.js',
       format: 'umd',
-      name: 'nice-util', // 替换为你的库名
+      name: 'NiceUtil', // 替换为你的库名
+      sourcemap: true,
       globals: {
         // 添加你的外部依赖，如 React: 'React'
-      },
-      sourcemap: true,
-      plugins: [terser()] // 压缩 UMD 文件
-    }
-  ],
-  plugins: [
-    nodeResolve(),
-    commonjs(),
-    typescript(),
-    json()
-  ],
-  preserveModules: false,
-};
+      }
+    },
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      typescript(),
+      json(),
+      terser()
+    ]
+  }
+];
